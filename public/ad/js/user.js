4a0a19cc76e9cc4c3d963f4031e37f1f11b2a13a
ZWJ <279909477@qq.com>
2018-09-19 21:52:19 +0800

    禁用功能

--- public/ad/js/user.js
+++ public/ad/js/user.js
@@ -1,4 +1,29 @@
 $(function(){
+    var isDelete;
+    getData();
+    $('tbody').on('click','#isTrue',function(){
+        var id = $(this).attr('data-id');
+        if($(this).text().trim() == '禁用'){
+            isDelete = 0;
+        }else{
+            isDelete = 1;
+        }
+        $.ajax({
+            url:'/user/updateUser',
+            type:'post',
+            data:{
+                id:id,
+                isDelete:isDelete
+            },
+            success:function(res){
+                if(res.success){
+                    getData();
+                }
+            }
+        })
+    })
+});
+function getData(){
     $.ajax({
         url:'/user/queryUser',
         type:'get',
@@ -11,4 +36,4 @@ $(function(){
             $('tbody').html(html);
         }
     })
-});
+}
